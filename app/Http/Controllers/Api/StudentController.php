44aae2f7294ebc925a1b5d929a4e057602f2f2ab
fiokee <smleffiong@gmail.com>
2025-05-07 11:41:48 +0100

    creating basic student api

---
+++ app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+       return response()->json(Student::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $data = $request->validate([
+            "name" => "required|string",
+            "email" => "required|email|unique:students,email",
+            "gender" => "required|in:male,female"
+        ]);
+
+        Student::create($data);
+
+        return response()->json([
+          "status" => true,
+          "message" => "Student created successfully"
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Student $student)
+    {
+        return response()->json([
+            "status" => true,
+            "message" => "Student Found",
+            "data" => $student
+        ]);
+
+
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
